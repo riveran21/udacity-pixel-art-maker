2dd2577da1ab95e6dfc0eaa762a47da1073f19a7
riveran21 <57384641+riveran21@users.noreply.github.com>
2019-11-21 16:27:01 -0600

    Update design.js
    Final update to the javascript file for Pixel Art Maker project.

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
 // Select color input
+function setColor() {
+  return document.getElementById('colorPicker').value;
+}
+
+
 // Select size input
+var gridSize = document.getElementById('sizePicker');
+gridSize.addEventListener('submit', function(event) {
+  event.preventDefault(); // Form not sent. Prevents screen refresh.
+  makeGrid(); // Calls makeGrid() Function
+})
+
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-
-// Your code goes here!
-
+  // Variables for Function
+  const gridHeight = document.getElementById('inputHeight').value;
+  const gridWidth = document.getElementById('inputWidth').value;
+  const canvas = document.getElementById('pixelCanvas');
+  canvas.innerHTML = ""; // Resets grid when new submitted again.
+  // Function below Creates Rows & Columns
+  for (let y = 0; y < gridHeight; ++y) {
+    var row = document.createElement('tr');
+    canvas.appendChild(row);
+    for (let x = 0; x <= gridWidth; ++x) {
+      var col = document.createElement('td');
+      row.appendChild(col);
+      // eventListener assigns color to background when cell clicked.
+      col.addEventListener('click', function(c) {
+        var color = setColor();
+        c.target.style.backgroundColor = color;
+      });
+    }
+  }
 }
